e426e8663d77ee1f5f6576ede3d02f33c27f988f
purvangdada <pdada2@illinois.edu>
2022-09-13 21:33:44 -0500

    added main folder

---
+++ robotic_arms_ws/build/CTestTestfile.cmake
@@ -0,0 +1,8 @@
+# CMake generated Testfile for 
+# Source directory: /home/dada/robotic_arms_ws/src
+# Build directory: /home/dada/robotic_arms_ws/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs(gtest)
+subdirs(bazu)
---
+++ robotic_arms_ws/build/bazu/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/dada/robotic_arms_ws/src/bazu
+# Build directory: /home/dada/robotic_arms_ws/build/bazu
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
